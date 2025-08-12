e64dd37940175a4dcd9981d470ac19a26ecc9230
Aditya <aditya@theironnetwork.org>
2025-08-12 18:30:36 +0530

    function name changed

--- detectors/detectors.go
+++ detectors/detectors.go
@@ -7,7 +7,7 @@ func RunAll(path string) {
 	DetectUnDefinedMessageKeys(path)
 	DetectCapitalVars(path)
 	DetectDeprecatedPackages(path)
-	DetectUnexportedFuncNames(path)
+	DetectExportedButInternalFuncs(path)
 	RunDeadCode(path)
 	// DetectFuncWithManyParams(path)
 }
